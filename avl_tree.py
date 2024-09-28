89fa9ed4a4e4d5c65fbcfb533a442d514c8c8cd3
Pathum312 <pathum.2019557@iit.ac.lk>
2024-09-28 14:13:51 +0530

    Changes after a while ;(

--- avl_tree.py
+++ avl_tree.py
@@ -33,6 +33,45 @@ class AVLTree:
             else:
                 self.insert(node=node.right, key=key)
     
+    def right_rotation(self, node: Node) -> None:
+        parent_node: Node = node
+        child_node: Node = node.left # type: ignore
+        
+        self.swap_parent(parent_node=parent_node, child_node=child_node)
+        
+        parent_node.left = None
+        parent_node.parent = child_node
+        
+        child_node.right = parent_node
+    
+    def left_rotation(self, node: Node) -> None:
+        parent_node: Node = node
+        child_node: Node = node.right # type: ignore
+        
+        self.swap_parent(parent_node=parent_node, child_node=child_node)
+        
+        parent_node.right = None
+        parent_node.parent = child_node
+        
+        child_node.left = parent_node
+    
+    def left_right_rotation(self, node: Node) -> None:
+        parent_node: Node = node
+        child_node: Node = node.left # type: ignore
+        
+        self.left_rotation(node=child_node)
+        self.right_rotation(node=parent_node)
+    
+    def swap_parent(self, parent_node: Node, child_node: Node) -> None:
+        if parent_node.root:
+            parent_node.root = False
+            
+            child_node.root = True
+            child_node.parent = None
+            self.node = child_node
+        else:
+            child_node.parent = parent_node.parent
+    
     def preorder_traversal(self, node: Node) -> None:
         node.balance_factor = self.calculate_balance_factor(node=node)
         print_node(node=node)
@@ -58,6 +97,7 @@ class AVLTree:
         
         return left_height - right_height
 
+# Print out all the details of a node
 def print_node(node: Node) -> None:
     print(f'key: {node.key}')
     print(f'left: {node.left.key if node.left else node.left}')
@@ -67,13 +107,14 @@ def print_node(node: Node) -> None:
     print(f'root: {node.root}\n')
 
 if __name__ == '__main__':
-    tree: AVLTree = AVLTree(key=20)
-    tree.insert(node=tree.node, key=10)
+    tree: AVLTree = AVLTree(key=10)
+    tree.insert(node=tree.node, key=20)
     tree.insert(node=tree.node, key=30)
-    tree.insert(node=tree.node, key=5)
-    tree.insert(node=tree.node, key=1)
-    tree.insert(node=tree.node, key=4)
-    tree.insert(node=tree.node, key=6)
-    tree.insert(node=tree.node, key=3)
     
+    print('Initial\n')
+    tree.preorder_traversal(node=tree.node)
+    
+    tree.left_rotation(node=tree.node)
+    
+    print('After rotation\n')
     tree.preorder_traversal(node=tree.node)
