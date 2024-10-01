7dec7817841b56dbe6fe6a59def0aa26eee1d917
Pathum312 <pathum.2019557@iit.ac.lk>
2024-10-01 12:39:20 +0530

    Added a logger

---
+++ logger.py
@@ -0,0 +1,55 @@
+from logging import (
+    DEBUG,
+    ERROR,
+    INFO,
+    FileHandler,
+    Formatter,
+    Logger,
+    StreamHandler,
+    getLogger,
+)
+
+
+def LOGGER(_name: str, _filename: str) -> Logger:
+    """
+    Used to log critical points in the code exexution.
+
+    Parameters:
+        _name (str): Name of the logger.
+        _filename (str): Name of the log file.
+
+    Returns:
+        Logger: An Logger object is returned.
+    """
+
+    # Create a logger for _name.
+    logger: Logger = getLogger(name=_name)
+    logger.setLevel(level=INFO)
+
+    # File handler, which logs even debug logs.
+    file_handler = FileHandler(filename=_filename)
+    file_handler.setLevel(level=DEBUG)
+
+    # Console handler with higher log level.
+    console_handler = StreamHandler()
+    console_handler.setLevel(level=ERROR)
+
+    # Format for the logs and is added to the handlers.
+    FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    formatter = Formatter(fmt=FORMAT)
+    file_handler.setFormatter(fmt=formatter)
+    console_handler.setFormatter(fmt=formatter)
+
+    # Add the handlers to the logger.
+    logger.addHandler(hdlr=file_handler)
+    logger.addHandler(hdlr=console_handler)
+
+    return logger
+
+
+if __name__ == "__main__":
+    logger: Logger = LOGGER(_name="testing", _filename="test.log")
+    logger.info(msg="Testing Start")
+    logger.info(msg="Testing...")
+    logger.error(msg="Testing Failed")
+    logger.info(msg="Testing Ended")
